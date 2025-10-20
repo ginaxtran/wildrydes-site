0a043180d3fdf5b43ea3517c7fffd9661dde9d5a
Gina Tran <102693106+ginaxtran@users.noreply.github.com>
2025-10-20 16:47:53 -0700

    Update AWS Cognito configuration for new region

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_0WplijLjy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3e0vvke6mig4jpvl83hjge71o0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-west-1_75LlcZlZN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4l9u8e1ss1coe88nhemggnm2p6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
