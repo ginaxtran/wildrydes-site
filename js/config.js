e445564ff08c06648f76f9448c39550ea16a8569
Gina Tran <102693106+ginaxtran@users.noreply.github.com>
2025-10-20 16:40:24 -0700

    Fix formatting in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_0WplijLjy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: ' 3e0vvke6mig4jpvl83hjge71o0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3e0vvke6mig4jpvl83hjge71o0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
