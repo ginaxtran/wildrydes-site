1ce3f7cfe90dacddf1ea19476d30be254f030db6
Gina Tran <102693106+ginaxtran@users.noreply.github.com>
2025-10-20 17:05:32 -0700

    Update Cognito user pool configuration

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_75LlcZlZN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4l9u8e1ss1coe88nhemggnm2p6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_lJvV9M2NZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '35177es77h907279apaqrnbp8u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
